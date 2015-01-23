c6347da2c3d3ef02fba7b5382189c0f8d66e372a
Devin Goodsell <devingoodsell@gmail.com>
2015-01-23 01:45:40 -0800

    Changed port to be dynamic

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 
 var
   express = require('./config/express'),
-  app = express()
+  app = express(),
+  port = process.env.PORT || 4444
 ;
 
-app.listen('4444', function() {
-  console.log('HTTP Server running on port 4444');
+app.listen(port, function() {
+  console.log('HTTP Server running on port ' + port);
 });
